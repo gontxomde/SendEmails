9c92f57b1afcbe3068856e90a6acd2c4f87ae8ef
Gonzalo Izaguirre <gontxomde@gmail.com>
2019-12-05 18:27:05 +0100

    Add logging to the application

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from mailer import Mailer
+import logging
+logging.basicConfig(filename='app.log',level = logging.INFO, filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 
 mailer = Mailer("emails.csv", toCaptcha="contra")
 mailer.process_file()
